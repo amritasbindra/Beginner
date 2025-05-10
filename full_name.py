4540e05700533447134454a500ff2e3a70ed42f0
Ritz <105677908+amritasbindra@users.noreply.github.com>
2025-05-09 22:01:15 -0700

    Create full_name.py

---
+++ full_name.py
@@ -0,0 +1,9 @@
+first_name = "spongebob"
+last_name = "squarepants"
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+message = (f"Hello {full_name.title()}! I'm ready!")
+
+print(message)
