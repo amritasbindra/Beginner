8f0386831e17913fe445b424c9e09122662b1081
Ritz <105677908+amritasbindra@users.noreply.github.com>
2025-05-09 21:33:52 -0700

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+print("Hello Python World!")
+
+#Variables
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+#try it yourself excerise begins
+
+sunrise = "I love seeing the sun rise while walking my dogs"
+print(sunrise)
+
+avatar_kyoshi = "I killed Chin the Conqueror"
+
+print(avatar_kyoshi)
+
+avatar_kyoshi = "I am melon lord"
+
+print (avatar_kyoshi)
+
+#try it yourself excerise ends
