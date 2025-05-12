4ae7519b391e7b09d9571998188ea8bd498d2f36
Ritz <105677908+amritasbindra@users.noreply.github.com>
2025-05-11 21:05:12 -0700

    Create namecases.py

---
+++ namecases.py
@@ -0,0 +1,11 @@
+#practice
+
+flying_bison = "appa"
+
+print(flying_bison.title())
+print(flying_bison.lower())
+print(flying_bison.upper())
+
+message = f"There once was a flying bison named {flying_bison.title()}. He travelled the world with his friends. {flying_bison.lower()} was also friends with a lemur named momo. I miss {flying_bison.upper()}"
+
+print(message)
