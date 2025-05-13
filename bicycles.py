bc9a5eaee2dc213e37e553ef7fb0f69078c482ed
Ritz <105677908+amritasbindra@users.noreply.github.com>
2025-05-12 21:16:00 -0700

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,19 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+print(bicycles[0].title())
+
+avatar_atla = ['aang', 'katara', 'sokka','zuko', 'azula', 'iroh']
+
+print(avatar_atla)
+
+print(avatar_atla[3].title())
+print(avatar_atla[2].title())
+
+print (f'What if {avatar_atla[3].title()} was the avatar? That would be interesting.')
+print (f'What if {avatar_atla[2].title()} was the avatar? That would be interesting.')
+print (f'What if {avatar_atla[1].title()} was the avatar? That would be interesting.')
+print (f'What if {avatar_atla[4].title()} was the avatar? That would be interesting.')
+print (f'What if {avatar_atla[5].title()} was the avatar? That would be interesting.')
+
---
+++ numbereight.py
@@ -0,0 +1,9 @@
+#practice
+
+print(7+1)
+
+print(8*1)
+
+print(18-10)
+
+print(16/2)
